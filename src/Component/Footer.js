dad9c95e297ee93318aac3ab8d38e5135b18ed9f
sagarbharvadiya <sagarbharvadiya@gmail.com>
2024-09-21 13:00:49 +0530

    portfolio

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -14,9 +14,9 @@ const Footer = () => {
             <p className='py-4'>
               We build killer Squarespace sites with our quick and pain-free process
             </p>
-            <div class="sqs-block button-block sqs-block-button" data-block-type="53" id="block-yui_3_17_2_1_1685733755740_4014"><div class="sqs-block-content" id="yui_3_17_2_1_1726774254249_483">
-              <div class="sqs-block-button-container sqs-block-button-container--left" data-animation-role="button" data-alignment="left" data-button-size="medium" data-button-type="primary" id="yui_3_17_2_1_1726774254249_482">
-                <a href="/meet-with-a-designer" class="sqs-block-button-element--medium sqs-button-element--primary sqs-block-button-element" data-initialized="true">
+            <div className="sqs-block button-block sqs-block-button" data-block-type="53" id="block-yui_3_17_2_1_1685733755740_4014"><div className="sqs-block-content" id="yui_3_17_2_1_1726774254249_483">
+              <div className="sqs-block-button-container sqs-block-button-container--left" data-animation-role="button" data-alignment="left" data-button-size="medium" data-button-type="primary" id="yui_3_17_2_1_1726774254249_482">
+                <a href="/meet-with-a-designer" className="sqs-block-button-element--medium sqs-button-element--primary sqs-block-button-element" data-initialized="true">
                   Meet with a Designer
                 </a>
               </div>
