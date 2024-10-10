14a7f8e759ba978912862d8873076169cbe597dc
vinodkeshariya <keshariyavinod793@gmil.com>
2024-10-10 18:24:17 +0530

    fix responsive

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <footer className="bg-[#0E0E0E] text-white">
       <div className="container container-lg mx-auto pt-14 pb-8">
-        <div className="row"> 
-          <div className='w-auto mx-auto'>
+        <div className="pl-2 md:pl-0 row"> 
+          <div className='w-full md:!w-auto mx-auto'>
             <a href={footerData.logo.link}>
               <img src={footerData.logo.src} alt={footerData.logo.alt} className="h-7" />
             </a>
@@ -17,15 +17,15 @@ const Footer = () => {
               our quick and pain-free process
             </p>
             <div className="sqs-block button-block sqs-block-button">
-              <div className="sqs-block-button-container container-lg sqs-block-button-container container-lg--left">
-                <Link to="/contact" className="inline-block text-white wgl-button relative px-8 py-3 z-1 font-semibold rounded-full transition-all duration-300 px-4 py-2 rounded-lg">
+              <div className="!pl-0 sqs-block-button-container container-lg sqs-block-button-container container-lg--left">
+                <Link to="/contact" className="mb-3 md:mb-0 inline-block text-white wgl-button relative px-8 py-3 z-1 font-semibold rounded-full transition-all duration-300 px-4 py-2 rounded-lg">
                   Get inTouch
                 </Link>
               </div>
             </div>
           </div>
 
-          <div className='!w-[68%] md:!w-auto mx-auto mt-[10px] md:!mt-0'>
+          <div className='w-full md:!w-auto mx-auto mt-[10px] md:!mt-0'>
             <h5 className="text-lg font-semibold mb-4">Navigation</h5>
             <ul className='pl-0'>
               {footerData.services.map((service, index) => (
@@ -36,7 +36,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className='w-auto mx-auto'>
+          <div className='w-full md:!w-auto mx-auto'>
             <h5 className="text-lg font-semibold mb-4">Contacts</h5>
             <ul className='pl-0'>
               <li className="mb-2">{footerData.contacts.address}</li>
